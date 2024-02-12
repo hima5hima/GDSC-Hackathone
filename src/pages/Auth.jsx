24ec134def644bf37a1ca871eb39afe44fdc3090
ZahraMohmed <zahramohmad346@gmail.com>
2024-02-12 03:58:48 +0200

    create design for Register

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import RegisterForm from "../components/register-form/RegisterForm";
+import Slider from "../components/slider/Slider.jsx";
 const Auth = () => {
-  return <div className="text-3xl font-bold p-8">Auth</div>;
+  return (
+    <>
+      <div className="container mx-auto  ">
+        <div className="grid   gap-4 lg:grid-cols-2 md:grid-cols-2 content-center sm:grid-cols-1  ">
+          <Slider />
+          <RegisterForm />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Auth;

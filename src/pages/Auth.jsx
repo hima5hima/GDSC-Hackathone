fc63f24d1d33e69e2acbc8f3c0d7a5fa4431497a
karimWG7 <karimghanem871@gmail.com>
2024-02-13 16:08:21 +0200

    this is another change

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,11 +1,10 @@
-
 import LoginForm from "../components/login-fom/LoginForm";
 import RegisterForm from "../components/register-form/RegisterForm";
 import Slider from "../components/slider/slider";
 const Auth = () => {
   return (
     <>
-      {/*  */}
+      {/* this is some changes */}
       <div className="container mx-auto grid grid-cols-2 gap-4 max-[900px]:grid-cols-1">
         <Slider />
         {/* condiation */}
